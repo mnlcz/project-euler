347455eb993b9e0504014aec269634b65365b61e
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-01-25 19:15:32 -0300

    Problem 2: first solution

---
+++ src/2.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Collection;
+
+require 'bootstrap.php';
+
+$limit = 4_000_000;
+
+$fibs = collect([2]);
+
+function fib(int $left, int $right, int $limit, Collection $fibs): void
+{
+    if ($right >= $limit)
+        return;
+
+    $sum = $left + $right;
+    $left = $right;
+    $right = $sum;
+
+    if ($right % 2 === 0)
+        $fibs->add($right);
+
+    fib($left, $right, $limit, $fibs);
+}
+
+fib(1, 2, $limit, $fibs);
+
+$sum = $fibs->sum();
+
+var_dump($sum);
