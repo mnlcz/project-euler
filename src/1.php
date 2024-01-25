dfadc36861ef14ceb616397ef0cb87366d3d6572
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-01-25 20:36:35 -0300

    Refactor

--- src/1.php
+++ src/1.php
@@ -2,6 +2,6 @@
 
 require 'bootstrap.php';
 
-$sum = collect(range(1, 999))->filter(fn($n) => $n % 3 === 0 || $n % 5 === 0)->sum();
+$sum = collect()->range(1, 999)->filter(fn($n) => $n % 3 === 0 || $n % 5 === 0)->sum();
 
 var_dump($sum);
