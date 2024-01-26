608acc1c579c704a22e62d3eba93950b4e9326f7
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-01-26 01:00:50 -0300

    Problem 6: solved

--- src/6.php
+++ src/6.php
@@ -12,3 +12,25 @@
 
 require 'bootstrap.php';
 
+use Illuminate\Support\Collection;
+
+function solve(Collection $range): int
+{
+    return square_of_sum($range) - sum_of_squares($range);
+}
+
+function sum_of_squares(Collection $range): int
+{
+    return $range->reduce(fn($carry, $item) => $carry + $item ** 2, 0);
+}
+
+function square_of_sum(Collection $range): int
+{
+    return $range->sum() ** 2;
+}
+
+$sample_range = collect()->range(1, 10);
+$real_range = collect()->range(1, 100);
+
+var_dump(solve($sample_range));
+var_dump(solve($real_range));
