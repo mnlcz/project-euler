d250cb015dffa90c29dc18d21400d758fd5238ec
MnLCz <mnlcz@tuta.io>
2026-06-07 18:18:09 -0300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+#############
+Project Euler
+#############
+
+Problemset provided by the website `Project Euler <https://projecteuler.net/about>`_.
---
+++ problems/1.scm
@@ -0,0 +1,29 @@
+#!/usr/bin/env -S guile3 -s
+!#
+
+#| MULTIPLES OF 3 OR 5
+  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
+3, 5, 6 and 9. The sum of these multiples is 23.
+
+  Find the sum of all the multiples of 3 and 5 below 1000.
+|#
+
+(use-modules
+  (ice-9 format))
+
+(define (euler1 limit-ni)
+  (let* ((nums (iota (- limit-ni 1) 1))
+        (filtered
+          (filter
+            (lambda (curr)
+              (or
+                (= 0 (modulo curr 5))
+                (= 0 (modulo curr 3))))
+              nums)))
+    (apply + filtered)))
+
+(let ((example (euler1 10)))
+  (format #t "Example result: ~a~%" example))
+
+(let ((real (euler1 1000)))
+  (format #t "Problem result: ~a~%" real))
